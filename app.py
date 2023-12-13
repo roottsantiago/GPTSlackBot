f904c9b447350e5a35f203a33042af1c19373690
Tomás Santiago <tsantiago@nomi.mx>
2023-12-12 20:48:05 -0600

    Refactor and improvement of Chat Completion for more updated models

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
 """
 App GPT Slack Bot
 """
-import os
 import logging
 import openai
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 from slack import WebClient
-
-
-SLACK_BOT_TOKEN = os.getenv("SLACK_BOT_TOKEN", None)
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", None)
-SLACK_APP_TOKEN = os.getenv("SLACK_APP_TOKEN", None)
+from chat.settings import (
+    SLACK_APP_TOKEN,
+    SLACK_BOT_TOKEN,
+    OPENAI_API_KEY,
+    MODEL,
+    CHUNK_SIZE
+)
+from chat.helpers import (
+    update_chat, 
+    chat_post_message,
+    get_conversation,
+    create_conversation
+)
 
 # Event API & Web API
 app = App(token=SLACK_BOT_TOKEN)
@@ -30,42 +37,37 @@ def handle_message_events(body, context):
         channel = body["event"]["channel"]
         thread_ts = body["event"].get("thread_ts", body["event"]["ts"])
         bot_user_id = context["bot_user_id"]
-        # Create prompt for ChatGPT
-        prompt = str(body["event"]["text"]).split(">")[1]
-        logging.info(prompt)
-
         # Let thre user know that we are busy with the request
-        response = client.chat_postMessage(
+        slack_response = app.client.chat_postMessage(
             channel=channel,
             thread_ts=thread_ts,
-            text=("Recibí tu solicitud :robot_face: \n"
-                "Espere por favor.")
+            text="Recibí tu solicitud :robot_face: \n Espere por favor..."
         )
-
+        message_ts = slack_response["message"]["ts"]
+        conversation = get_conversation(app, channel, thread_ts)
+        messages = create_conversation(conversation, bot_user_id)
         # Check ChatGPT
-        response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=prompt,
-            max_tokens=1024,
-            n=1,
-            stop=None,
-            temperature=0.5
-        ).choices[0].text
-
-        # Reply to thread
-        response = client.chat_postMessage(
-            channel=channel,
-            thread_ts=thread_ts,
-            text=f"Aquí tienes: \n{response}"
+        response = openai.ChatCompletion.create(
+            model=MODEL,
+            messages=messages,
+            stream=True
         )
+        # Reply to thread
+        response_text = ""
+        count = 0
+        for chunk in response:
+            if chunk.choices[0].delta.get("content"):
+                count += count
+                response_text += chunk.choices[0].delta.content
+                if count > CHUNK_SIZE:
+                    update_chat(app, channel, message_ts, response_text)
+                    count = 0
+            elif chunk.choices[0].finish_reason == "stop":
+                update_chat(app, channel, message_ts, response_text)
 
     except Exception as exc:
         logging.info(f"Error: {exc}")
-        app.client.chat_postMessage(
-            channel=channel,
-            thread_ts=thread_ts,
-            text=f"Hubo un error, no puedo dar una respuesta."
-        )
+        chat_post_message(app, channel, thread_ts)
 
 
 if __name__ == "__main__":
